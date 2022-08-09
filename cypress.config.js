4ecfb84e9c4148e116576edade9767c4812ca4d1
Eduardo <tatotux@pm.me>
2022-08-09 08:40:25 -0600

    Adding Jenkinsfile to POC

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: false,
     setupNodeEvents(on, config) {
         on('before:browser:launch', (browser = {}, launchOptions) => {
           // `args` is an array of all the arguments that will
